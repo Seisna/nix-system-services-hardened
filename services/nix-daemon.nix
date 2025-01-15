4294c984ddb125e77012ca3084f4b7a21c61b08e
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-15 18:04:26 +0100

    fix: kernel access issue with some packages

--- services/nix-daemon.nix
+++ services/nix-daemon.nix
@@ -2,10 +2,7 @@
   systemd.services.nix-daemon.serviceConfig = {
     NoNewPrivileges = true;
     ProtectControlGroups = true;
-    ProtectHostname = true;
-    ProtectKernelTunables = true;
     ProtectKernelModules = true;
-    ProtectKernelLogs = true;
     PrivateMounts = true;
     PrivateTmp = true;
     PrivateDevices = true;
@@ -19,6 +16,15 @@
       "AF_INET"
     ];
     CapabilityBoundingSet= [ 
+      "~CAP_SYS_CHROOT"
+      "~CAP_BPF"
+      "~CAP_AUDIT_WRITE"
+      "~CAP_AUDIT_CONTROL"
+      "~CAP_AUDIT_READ"
+      "~CAP_SYS_PTRACE"
+      "~CAP_SYS_NICE"
+      "~CAP_SYS_RESOURCE"
+      "~CAP_SYS_RAWIO"
       "~CAP_SYS_TIME" 
       "~CAP_SYS_PACCT" 
       "~CAP_LINUX_IMMUTABLE" 
